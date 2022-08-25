2b7917bbd6d800b6ae6b3c11e0aebb2c00e89332
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-08-25 22:52:47 +0530

    check the presence of +

--- calculator.dart
+++ calculator.dart
@@ -4,9 +4,15 @@ void main() {
   print("========CALCULATOR========");
   var input = stdin.readLineSync();
   var check = input?.contains(new RegExp(r'^[0-9\-=/*\+]+$'));
-  if (check == false){
+  if (check == false) {
     print("ERROR:Only inputs Expected");
     exit(0);
-  };
-  var number = input;
+  }
+  var number = 0;
+  input?.runes.forEach((element) {
+    var digit = new String.fromCharCode(element);
+    if (digit == '+') {
+      print('Adding');
+    }
+  });
 }
