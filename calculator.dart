6ca289988cce3d5f9c6e3eb017cfb9a2176640aa
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-08-26 18:17:37 +0530

    create a list of numbers

--- calculator.dart
+++ calculator.dart
@@ -8,7 +8,7 @@ void main() {
     print("ERROR:Only numerical inputs are Expected");
     exit(0);
   }
-  var number;
+  var numbers = new List.empty(growable: true);
   var hasOperation = input?.contains(new RegExp(r'^[0-9]+$'));
   if (hasOperation == true) {
     print("ERROR :NO OPERATION SPECIFIED");
@@ -18,10 +18,12 @@ void main() {
     input = input?.substring(0, input.length - 1);
   }
   int i = 0;
+  int op = 0;
   input?.runes.forEach((element) {
     var digit = new String.fromCharCode(element);
     if (digit == '+') {
-      number = input?.substring(0, i);
+      numbers.add(input?.substring(op, i));
+      op = i + 1;
     } else if (digit == '-') {
       print('Subtractting');
     } else if (digit == '*') {
@@ -31,5 +33,5 @@ void main() {
     }
     i++;
   });
-  print(number);
+  print(numbers);
 }
