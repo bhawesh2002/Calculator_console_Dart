31e2251de9469d9d2baf1ef274fced854bb6ebd8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-08-26 21:04:14 +0530

    create a list of operations to be performed.

--- calculator.dart
+++ calculator.dart
@@ -23,25 +23,31 @@ void main() {
   var numbers = new List.empty(growable: true);
   int i = 0;
   int op_index = 0;
+  var operations = new List<String>.empty(growable: true);
   input?.runes.forEach((element) {
     var digit = new String.fromCharCode(element);
     if (digit == '+') {
       numbers.add(input?.substring(op_index, i));
       op_index = i + 1;
+      operations.add('+');
     } else if (digit == '-') {
       numbers.add(input?.substring(op_index, i));
       op_index = i + 1;
+      operations.add('-');
     } else if (digit == '*') {
       numbers.add(input?.substring(op_index, i));
       op_index = i + 1;
+      operations.add('*');
     } else if (digit == '/') {
       numbers.add(input?.substring(op_index, i));
       op_index = i + 1;
+      operations.add('/');
     }
     i++;
   });
   numbers.add(input?.substring(op_index, i));
   print(numbers);
+  print(operations);
   /*for (var i = 0; i < numbers.length; i++) {
     numbers[i] = int.parse(numbers[i]);
   }
