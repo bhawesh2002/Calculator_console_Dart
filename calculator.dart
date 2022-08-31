89cf996c92d8abdf85a0f3731e9e54272604b130
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-08-31 21:27:37 +0530

    comment the code

--- calculator.dart
+++ calculator.dart
@@ -1,96 +1,143 @@
 import 'dart:io';
 
 void main() {
-  print("========CALCULATOR========");
-  var input = stdin.readLineSync();
-  var check = input?.contains(new RegExp(r'^[0-9\-=/*\+]+$'));
+  print("========CALCULATOR========"); // print the title of the calculator
+  var input = stdin.readLineSync(); //get the input as string from the user
+  var check = input?.contains(new RegExp(
+      r'^[0-9\-=/*\+]+$')); //check if the input is valid and only contains numbers and operators
   if (check == false) {
-    print("ERROR:Only numerical inputs are Expected");
+    print(
+        "ERROR:Only numerical inputs are Expected"); //if the input is not valid print error message and exit the program
     exit(0);
   }
-  var hasOperation = input?.contains(new RegExp(r'^[0-9]+$'));
+  var hasOperation = input?.contains(
+      new RegExp(r'^[0-9]+$')); //check if the input has any operators
   if (hasOperation == true) {
-    print("ERROR :NO OPERATION SPECIFIED");
+    print(
+        "ERROR :NO OPERATION SPECIFIED"); //if the input has operators print error message and exit the program
     exit(0);
   }
-  var numbers = new List.empty(growable: true);
-  int i = 0;
-  int op_index = 0;
-  var operations = new List<String>.empty(growable: true);
+  var numbers = new List.empty(
+      growable: true); //create a list to store the numbers in the input
+  int i = 0; //create a counter to store the index of the numbers in the list
+  int op_index =
+      0; //create a counter to store the index of the operators in the list
+  var operations = new List<String>.empty(
+      growable: true); //create a list to store the operators in the input
   input?.runes.forEach((element) {
-    var digit = new String.fromCharCode(element);
+    //loop through the input and store the numbers and operators in the lists
+    var digit = new String.fromCharCode(
+        element); //get the character at the index of the loop
     if (digit == '+') {
-      numbers.add(input.substring(op_index, i));
-      op_index = i + 1;
-      operations.add('+');
+      //if the character is an operator add it to the list of operators
+      numbers.add(input.substring(op_index,
+          i)); //add the number before the operator to the list of numbers
+      op_index = i + 1; //update the index of the operator
+      operations.add('+'); //add the operator to the list of operators
     } else if (digit == '-') {
-      numbers.add(input.substring(op_index, i));
-      op_index = i + 1;
-      operations.add('-');
+      //if the character is an operator add it to the list of operators
+      numbers.add(input.substring(op_index,
+          i)); //add the number before the operator to the list of numbers
+      op_index = i + 1; //update the index of the operator
+      operations.add('-'); //add the operator to the list of operators
     } else if (digit == '*') {
-      numbers.add(input.substring(op_index, i));
-      op_index = i + 1;
-      operations.add('*');
+      //if the character is an operator add it to the list of operators
+      numbers.add(input.substring(op_index,
+          i)); //add the number before the operator to the list of numbers
+      op_index = i + 1; //update the index of the operator
+      operations.add('*'); //add the operator to the list of operators
     } else if (digit == '/') {
-      numbers.add(input.substring(op_index, i));
-      op_index = i + 1;
-      operations.add('/');
+      //if the character is an operator add it to the list of operators
+      numbers.add(input.substring(op_index,
+          i)); //add the number before the operator to the list of numbers
+      op_index = i + 1; //update the index of the operator
+      operations.add('/'); //add the operator to the list of operators
     }
-    i++;
+    i++; //update the index of the loop
   });
-  numbers.add(input?.substring(op_index, i));
+  numbers.add(input?.substring(op_index,
+      i)); //add the number after the last operator to the list of numbers
   for (var i = 0; i < numbers.length; i++) {
-    numbers[i] = int.parse(numbers[i]);
+    //loop through the list of numbers
+    numbers[i] = int.parse(numbers[i]); //convert the string to int
   }
-  var div;
-  var mul;
-  var sum;
-  print(numbers);
-  print(operations);
+  var div; //create a variable to store the result of the division
+  var mul; //create a variable to store the result of the multiplication
+  var sum; //create a variable to store the result of the sum
+  print(numbers); //print the list of numbers
+  print(operations); //print the list of operators
   while (true) {
     if (operations.contains('/') == true) {
-      int indexVal = operations.indexOf('/');
-      div = numbers[indexVal] / numbers[indexVal + 1];
-      numbers.removeAt(indexVal);
-      numbers[indexVal] = div;
-      operations.removeAt(indexVal);
+      //if the list of operators contains division
+      int indexVal =
+          operations.indexOf('/'); //get the index of the division operator
+      div = numbers[indexVal] /
+          numbers[indexVal +
+              1]; //divide the numbers at the index of the division operator
+      numbers.removeAt(
+          indexVal); //remove the numbers at the index of the division operator
+      numbers[indexVal] =
+          div; //replace the number at the index of the division operator with the result of the division
+      operations.removeAt(
+          indexVal); //remove the operator at the index of the division operator
     } else {
-      break;
+      break; //if the list of operators does not contain division break the loop
     }
   }
   while (true) {
     if (operations.contains('*') == true) {
-      int indexVal = operations.indexOf('*');
-      mul = numbers[indexVal] * numbers[indexVal + 1];
-      numbers.removeAt(indexVal);
-      numbers[indexVal] = mul;
-      operations.removeAt(indexVal);
+      //if the list of operators contains multiplication
+      int indexVal = operations
+          .indexOf('*'); //get the index of the multiplication operator
+      mul = numbers[indexVal] *
+          numbers[indexVal +
+              1]; //multiply the numbers at the index of the multiplication operator
+      numbers.removeAt(
+          indexVal); //remove the numbers at the index of the multiplication operator
+      numbers[indexVal] =
+          mul; //replace the number at the index of the multiplication operator with the result of the multiplication
+      operations.removeAt(
+          indexVal); //remove the operator at the index of the multiplication operator
     } else {
-      break;
+      break; //if the list of operators does not contain multiplication break the loop
     }
   }
   while (true) {
     if (operations.contains('+') == true) {
-      int indexVal = operations.indexOf('+');
-      sum = numbers[indexVal] + numbers[indexVal + 1];
-      numbers.removeAt(indexVal);
-      numbers[indexVal] = sum;
-      operations.removeAt(indexVal);
+      //if the list of operators contains addition
+      int indexVal =
+          operations.indexOf('+'); //get the index of the addition operator
+      sum = numbers[indexVal] +
+          numbers[indexVal +
+              1]; //add the numbers at the index of the addition operator
+      numbers.removeAt(
+          indexVal); //remove the numbers at the index of the addition operator
+      numbers[indexVal] =
+          sum; //replace the number at the index of the addition operator with the result of the addition
+      operations.removeAt(
+          indexVal); //remove the operator at the index of the addition operator
     } else {
-      break;
+      break; //if the list of operators does not contain addition break the loop
     }
   }
   while (true) {
     if (operations.contains('-') == true) {
-      int indexVal = operations.indexOf('-');
-      mul = numbers[indexVal] - numbers[indexVal + 1];
-      numbers.removeAt(indexVal);
-      numbers[indexVal] = mul;
-      operations.removeAt(indexVal);
+      //if the list of operators contains subtraction
+      int indexVal =
+          operations.indexOf('-'); //get the index of the subtraction operator
+      mul = numbers[indexVal] -
+          numbers[indexVal +
+              1]; //subtract the numbers at the index of the subtraction operator
+      numbers.removeAt(
+          indexVal); //remove the numbers at the index of the subtraction operator
+      numbers[indexVal] =
+          mul; //replace the number at the index of the subtraction operator with the result of the subtraction
+      operations.removeAt(
+          indexVal); //remove the operator at the index of the subtraction operator
     } else {
-      break;
+      break; //if the list of operators does not contain subtraction break the loop
     }
   }
-  print(numbers);
-  print(operations);
+  print(numbers); //print the list of numbers
+  print(operations); //print the list of operators
 }
